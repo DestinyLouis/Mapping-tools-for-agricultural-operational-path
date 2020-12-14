64a0ed1cf401b99a6cf271295e917ee623a75df1
DestinyLouis <74294608+DestinyLouis@users.noreply.github.com>
2020-12-14 23:22:43 +0800

    Delete txt.py

--- txt.py
+++
@@ -1,5 +0,0 @@
-# <!--{% import "bootstrap/wtf.html" as wtf  %}-->
-# <!--<form method="post">-->
-# <!--  {{wtf.quickform(form).lands_file.label}}{{wtf.quickform(form).lands_file}}-->
-# <!--  {{wtf.quickform(form).submit}}-->
-# <!--</form>-->
